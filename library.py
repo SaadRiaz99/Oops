28e1a7d8892d8bf191f7f5bbdea80f0fb7d3ca2f
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-12 16:56:07 +0500

    Add Book Init

--- library.py
+++ library.py
@@ -3,3 +3,25 @@ class Book:
         self.title = title
         self.author = author
         self.issued_book = False
+
+
+class memebet():
+    def __init__(self, memeber_id , book_name):
+        self.member_id = memeber_id
+        self.book_name = book_name
+        self.borrowed_book = []
+
+class library():
+    def __init__(self ):
+        self.book = []        
+        self.memeber = []
+
+
+
+    def add_book(self , book):
+        self.book.append = book
+        print("Book Added Sucessfully") 
+
+    def add_member(self , member):
+        self.member.append = member
+        print("Member Added Sucessfully") 
